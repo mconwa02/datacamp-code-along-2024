15b6b34098ed83341e81b7c00ae65b562fc48a6a
mconwa02 <mconwa02@dcs.bbk.ac.uk>
2024-06-30 19:18:21 +0100

    adding final tests

--- production_code.py
+++ production_code.py
@@ -1,4 +1,4 @@
-from pandas import DataFrame
+from pandas import DataFrame, Series
 
 from sample_code import df
 
@@ -15,31 +15,35 @@ def customers_mean_age(df: DataFrame) -> DataFrame:
     return df
 
 
-# def filer_customer_items(df: DataFrame) -> DataFrame:
-#     """ insert a good doc string"""
-#     df["annual_duration_flat"] = [
-#         item for sublist in df["annual_duration"].tolist() for item in sublist
-#     ]
-#     return df
-#
-#
-# def join_customer_names(df: DataFrame) -> DataFrame:
-#     """ insert a good doc string"""
-#     df["campaign_limit_concat"] = (
-#         "".join(str(item) for item in df["campaign_limit"].tolist()),
-#     )
-#     return df
+def gb_max_customer_account_balance(df: DataFrame) -> DataFrame:
+    """Adds a column with the maximum account balance to the DataFrame."""
+    df["balance_max"] = df["balance"].max()
+    return df["balance_max"].iloc[0]
+
+
+def gb_customers_mean_age(df: DataFrame) -> DataFrame:
+    """Adds a column with the mean age of all customers to the DataFrame."""
+    df["age_mean"] = df["age"].mean()
+    return df["age_mean"].iloc[0]
+
+
+def marital_status_groups(df: DataFrame) -> DataFrame:
+    return df.groupby("marital").apply(
+        lambda x: Series(
+            {
+                "balance_max": gb_max_customer_account_balance(x),
+                "age_mean": gb_customers_mean_age(x),
+            }
+        ),
+        include_groups=False,
+    )
 
 
 def creating_features_banking_data(df):
-    return (
-        df.pipe(max_customer_account_balance).pipe(customers_mean_age)
-        # todo fix broken pipe functions
-        # .pipe(filer_customer_items)
-        # .pipe(join_customer_names)
-    )
+    return df.pipe(max_customer_account_balance).pipe(customers_mean_age)
 
 
 if __name__ == "__main__":
     banking_customers_df = creating_features_banking_data(df)
     print(banking_customers_df.head())
+    print(marital_status_groups(df))
